66b42f1572d5d0faeeb5d661d13c00eb66672aed
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-07-03 08:20:53 +0530

    [Girish]comparing two lines using classes and methods

--- Line comparison/Program.cs
+++ Line comparison/Program.cs
@@ -9,7 +9,7 @@ namespace Line_comparison
             Console.WriteLine("Welcome to Line Comparision Computation Problem");
             //initialising coordinates as integers
             int a1, a2, b1, b2, x1, x2, y1, y2;
-            double lineOneLength, lineTwoLength;
+       
             Console.WriteLine("Enter a-coordinate of First Point of First Line:");
             //converting into int using ToInt32
             a1 = Convert.ToInt32(Console.ReadLine());
@@ -33,23 +33,38 @@ namespace Line_comparison
             Console.WriteLine("Enter b-coordinate of Second Point Second Line:");
             y2 = Convert.ToInt32(Console.ReadLine());
 
-            //formula for calculate length
-            lineOneLength = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
-            lineTwoLength = Math.Sqrt(Math.Pow(x2 - a1, 2) + Math.Pow(y2 - y1, 2));
+            double Length_of_first_Line = FindLength(a1, b2, a1, b2);
 
-            int result = lineOneLength.CompareTo(lineTwoLength);
-            if (result < 0)
+            double Length_of_second_Line = FindLength(x1, y2, x1, y2);
+
+            Console.WriteLine("\nFirst line length is : " + Length_of_first_Line);
+
+            Console.WriteLine("\nSecond line length is : " + Length_of_second_Line);
+
+            if (Length_of_first_Line.Equals(Length_of_second_Line))
             {
-                Console.WriteLine("Line 1 is less than line 2");
-            }
-            else if (result == 0)
-            {
-                Console.WriteLine("Line 1 is equal to line 2");
+                Console.WriteLine("\nBoth Lines Are Equal");
             }
             else
             {
-                Console.WriteLine("Line 1 is greater than line 2");
+                Console.WriteLine("\nBoth Lines Are Not Equal");
             }
+            switch (Length_of_first_Line.CompareTo(Length_of_second_Line))
+            {
+                case 1:
+                    Console.WriteLine("\nFirst Line Is Greater Than Second Line");
+                    break;
+                case -1:
+                    Console.WriteLine("\n Second Line Is Greater Than First Line");
+                    break;
+                default:
+                    Console.WriteLine("\nBoth  Lines are Equal");
+                    break;
+            }
+        }
+        private static double FindLength(int a1, int a2, int b1, int b2)
+        {
+            return Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
         }
     }
 }
